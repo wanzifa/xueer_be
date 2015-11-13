9da22f18dd1798d8a034fd1467cc27a85d08fe1a
wanzifa <773857626@qq.com>
2015-11-13 17:12:05 +0800

    update teachers.py

--- xueer/api_1_0/teachers.py
+++ xueer/api_1_0/teachers.py
@@ -5,5 +5,5 @@ from ..models import Teachers
 
 @api.route('teachers/<int:id>')
 def get_teacher_id():
-    teacher = Teacher.query.get_or_404(id)
+    teacher = Teachers.query.get_or_404(id)
     return jsonify(teacher.to_json())
