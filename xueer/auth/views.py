339e6c2edec7b7296039fc7fdffab525ab3f5890
neo1218 <neo1218@yeah.net>
2015-11-26 19:49:12 +0800

    change is_authenticated() to is_authenticated

--- xueer/auth/views.py
+++ xueer/auth/views.py
@@ -9,7 +9,7 @@ from .forms import LoginForm, RegistrationForm
 
 @auth.before_app_request
 def before_request():
-    if current_user.is_authenticated():
+    if current_user.is_authenticated:
         current_user.ping()
 
 
