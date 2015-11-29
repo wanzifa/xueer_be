891b70a3f849d9904c178de1e2a9141891d72697
neo1218 <neo1218@yeah.net>
2015-11-29 11:00:49 +0800

    update shell script

--- data.sh
+++ data.sh
@@ -12,3 +12,17 @@ python manage.py db upgrade
 
 # create user Roles
 python manage.py shell
+
+# adduser
+echo "add user neo1218"
+echo "please input password and confirm the password"
+python manage.py adduser neo1218 neo1218@yeah.net
+
+# done!
+echo "\n"
+echo "----------------------------------"
+echo "create test database done!"
+echo "test database: xueer_dev.sqlite"
+echo "test user: neo1218"
+echo "test user email: neo1218@yeah.net"
+echo "----------------------------------"
