cff0c4a787b0616c516e969c2b193ba9d2cbc287
wanzifa <773857626@qq.com>
2015-12-10 20:34:41 +0800

    changes of tips

--- xueer/api_1_0/tips.py
+++ xueer/api_1_0/tips.py
@@ -14,13 +14,88 @@
 """
 # 挖坑
 # 填坑
-from xueer.api_1_0 import api
+from flask import jsonify, url_for, request, current_app
+from sqlalchemy import desc
+from ..models import Tips
+from . import api
+from xueer import db
+import json
+from xueer.api_1_0.authentication import auth
 
 
-@api.route('/tips/', methods=["GET"])
+@api.route('/tips', methods=["GET"])
 def get_tips():
     """
     获取首页的每日tip
     :return:
     """
-    pass
+    page = request.args.get('page', 1, type=int)
+    pagination = Tips.query.order_by(Tips.timestamp.desc()).paginate(
+        page, per_page=current_app.config['XUEER_TIPS_PER_PAGE'],
+        error_out=False
+    )
+    tips = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for('api.get_tips', page=page-1, _external=True)
+    next = None
+    if pagination.has_next:
+        next = url_for('api.get_tips', page=page-1, _external=True)
+    return jsonify({
+        'posts': [tip.to_json() for tip in tips],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
+
+
+@api.route('/tip/<int:id>', methods=['GET'])
+def get_tip_id(id):
+    """
+    获取已知id对应的tip
+    :return:
+    """
+    tip = Tips.query.get_or_404(id)
+    return jsonify(
+        tip.to_json()
+    )
+
+
+@api.route('/tip/<int:id>', methods=['GET', 'POST'])
+@auth.login_required
+def new_tip(id):
+    """
+    发布一条新贴士
+    :param id: 贴士id
+    :return:
+    """
+    tip = Tips.from_json(request.json)
+    db.session.add(tip)
+    db.session.commit()
+    return jsonify(
+        tip.to_json()), 201, {
+               'Location': url_for(
+                    'api.get_tip_id',
+                    id=tip.id, _external=True
+               )
+           }
+
+
+@api.route('/tip/<int:id>', methods=['GET', 'DELETE'])
+@auth.login_required
+def delete_tip(id):
+    """
+    删除一个贴士
+    :param id: 贴士id
+    :return:
+    """
+    tip = Tips.query.get_or_404(id)
+    db.session.delete(tip)
+    db.session.commit()
+    return jsonify({
+        'message': '该贴士已被删除'
+    })
+
+
+
+
