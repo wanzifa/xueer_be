f0da04cd0353e0b50b5c3cecba74ef6add110870
neo1218 <neo1218@yeah.net>
2015-12-22 16:15:02 +0800

    解决冲突

--- xueer/__init__.py
+++ xueer/__init__.py
@@ -21,24 +21,12 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 moment = Moment(app)
 
-<<<<<<< HEAD
-    # regist blueprint
-    from hello import hello
-    app.register_blueprint(hello, url_prefix='/hello')
-
-    from auth import auth
-    app.register_blueprint(auth, url_prefix='/auth')
-
-    from api_1_0 import api
-    app.register_blueprint(api, url_prefix='/api')
-=======
 
 from hello import hello
 app.register_blueprint(hello)
 
 from auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
->>>>>>> a73d146f808268e47822f03b8727d286dd637d4d
 
 from api_1_0 import api as api_1_0_blueprint
 app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
