bb3cee87f9f54909edce32445835a8a14f286a7e
wanzifa <773857626@qq.com>
2015-12-27 15:55:53 +0800

    improve search

--- xueer/api_1_0/search.py
+++ xueer/api_1_0/search.py
@@ -196,6 +196,103 @@ def get_search():
 
 
 
-#@api.route('/search/prefetch/<string:keywords>')
-#def get_research2(keywords):
+@api.route('/search/prefetch/', methods=['GET', 'POST'])
+def get_research2(keywords):
+    keywords = request.args.get('keywords')
+    if request.methods == 'POST':
+        if request.args.get('keywords'):
+            keywords = request.args.get('keywords')
+        if request.args.get('sort') == 'view':
+            if request.args.get('main_cat'):
+                if request.args.get('ts_cat'):
+                    course1 = Courses.query.whoosh_search(keywords).filter_by(
+                        type_id=request.args.get('ts_cat'),
+                        category_id=request.args.get('main_cat')
+                        ).all()
+                    tags = Tags.query.whoosh_search(keywords).all()
+                    course2 = []
+                    for tag in tags:
+                        tc=tag.courses.filter_by(
+                        type_id=request.args.get('ts_cat'),
+                        category_id=request.args.get('main_cat')
+                        ).all()
+                    course2 += tc
+                    course0 = course1 + course2
+                    courses =sorted(course0,  key=lambda course : course.count, reverse=True)
+
+                else:
+                    course1 = Courses.query.whoosh_search(keywords).filter_by(
+                    category_id=request.args.get('main_cat')).all()
+                    tags = Tags.query.whoosh_search(keywords).all()
+                    course2 = []
+                    for tag in tags:
+                        tc = tag.courses.filter_by(
+                        category_id=request.args.get('main_cat')
+                        ).all()
+                        course2 += tc
+                    course0 = course1+course2
+                    courses =sorted(course0,  key=lambda course : course.count, reverse=True)
+
+            else:
+                course1 = Courses.query.whoosh_search(keywords).all()
+                tags = Tags.query.whoosh_search(keywords).all()
+                course2 = []
+                for tag in tags:
+                    tc=tag.courses.all()
+                    course2 += tc
+                course0 = course1 + course2
+                courses =sorted(course0,  key=lambda course : course.count, reverse=True)
+
+        elif request.args.get('sort') == 'like':
+            if request.args.get('main_cat'):
+                if request.args.get('ts_cat'):
+                    course1 = Courses.query.whoosh_search(keywords).filter_by(
+                        type_id=request.args.get('ts_cat'),
+                        category_id=request.args.get('main_cat')
+                    ).all()
+                    tags = Tags.query.whoosh_search(keywords).all()
+                    course2 = []
+                    for tag in tags:
+                        tc = tag.courses.filter_by(
+                        type_id=request.args.get('ts_cat'),
+                        category_id=request.args.get('main_cat')
+                    ).all()
+                        course2 += tc
+                    course0 = course1 + course2
+                    courses =sorted(course0,  key=lambda course : course.likes, reverse=True)
+                else:
+                    course1 = Courses.query.whoosh_search(keywords).filter_by(
+                            category_id=request.args.get('main_cat')).all()
+                    tags = Tags.query.whoosh_search(keywords).all()
+                    course2 = []
+                    for tag in tags:
+                        tc = tag.courses.filter_by(
+                        category_id=request.args.get('main_cat')
+                    ).all()
+                        course2 += tc
+                    course0 = course1 + course2
+                    courses =sorted(course0,  key=lambda course : course.likes, reverse=True)
+            else:
+                course1 = Courses.query.whoosh_search(keywords).all()
+                tags = Tags.query.whoosh_search(keywords).all()
+                course2 = []
+                for tag in tags:
+                    tc=tag.courses.all()
+                    course2 += tc
+                course0 = course1 + course2
+                courses =sorted(course0,  key=lambda course : course.likes, reverse=True)
+        else:
+            course1 = Courses.query.whoosh_search(keywords).all()
+            tags = Tags.query.whoosh_search(keywords).all()
+            course2 = []
+            for tag in tags:
+                tc=tag.courses.all()
+                course2 += tc
+            courses = course1 + course2
+        courses = courses[0:5]
+        return json.dumps(
+            [course.to_json2() for course in courses],
+            ensure_ascii=False,
+            indent=1
+        ), 200
 
