3215c5504a729f79ca36a8b175344330040a465a
wanzifa <773857626@qq.com>
2016-01-14 16:38:07 +0800

    a better method to avoid repeating

--- xueer/models.py
+++ xueer/models.py
@@ -759,18 +759,24 @@ def save():
         str = '/'.join(seg_list)
         results = str.split('/')
         for result in results:
-            s = Search(name=result)
-            s.courses.append(course)
-            db.session.add(s)
-            db.session.commit()
+            if(Search.query.filter_by(name=result) == None):
+                s = Search(name=result)
+            elif(course not in Search.query.filter_by(name=result).first().courses.all()):
+                s = Search.query.filter_by(name=result).first()
+                s.courses.append(course)
+                db.session.add(s)
+                db.session.commit()
 
     for tag in Tags.query.all():
         seg_list = jieba.cut_for_search(tag.name)
         str = '/'.join(seg_list)
         results = str.split('/')
         for result in results:
-            s = Search(name=result)
-            s.tags.append(tag)
-            db.session.add(s)
-            db.session.commit()
+            if(Search.query.filter_by(name=result) == null):
+                s = Search(name=result)
+            elif(tag not in Search.query.filter_by(name=result).first().tags.all()):
+                s = Search.query.filter_by(name=result).first()
+                s.tags.append(tag)
+                db.session.add(s)
+                db.session.commit()
 
