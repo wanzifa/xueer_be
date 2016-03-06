fa94f5666ccf4af41334230670bac471d976ff18
neo1218 <neo1218@yeah.net>
2016-03-06 13:49:06 +0800

    add route placehold

--- xueer/hello/views.py
+++ xueer/hello/views.py
@@ -7,9 +7,6 @@ from jinja2 import Environment
 from . import hello
 
 
-# hello.jinja_env.add_extension('jinja2.ext.loopcontrols')
-
-
 def is_mobie():
     platform = request.user_agent.platform
     if platform in ["android", "iphone", "ipad"]:
@@ -25,10 +22,65 @@ def index():
     else:
         return render_template("hello/desktop/index.html")
 
+# placehold 路由
+@hello.route('/search_result/', methods=['GET'])
+def get_search_result():
+    """get search result"""
+    if is_mobie():
+        return render_template("hello/mobile/index.html")
+    else:
+        return render_template("hello/desktop/index.html")
 
-# @hello.route('/course')
-# Def course():
-#     with hello.open_resource('mock/course.json') as f:
-#         data = f.read()
-#         json_dict = json.loads(data)
-#     return render_template("pages/course.html",info=json_dict['info'],hot_comments=json_dict['hot_comments'])
+
+@hello.route('/course/<int:id>/', methods=['GET'])
+def course(id):
+    """ course index  """
+    if is_mobie():
+        return render_template("hello/mobile/index.html")
+    else:
+        return render_template("hello/desktop/index.html")
+
+@hello.route('/tip/<int:id>/', methods=['GET'])
+def tip(id):
+    """ tip index """
+    if is_mobie():
+        return render_template("hello/mobile/index.html")
+    else:
+        return render_template("hello/desktop/index.html")
+
+
+@hello.route('/courses/', methods=['GET'])
+def courses():
+    """ get courses """
+    if is_mobie():
+        return render_template("hello/mobile/index.html")
+    else:
+        return render_template("hello/desktop/index.html")
+
+
+@hello.route('/user/<string:username>/', methods=['GET'])
+def user(username):
+    """ user index """
+    if is_mobie():
+        return render_template("hello/mobile/index.html")
+    else:
+        return render_template("hello/desktop/index.html")
+
+
+@hello.route('/login/', methods=['GET', 'POST'])
+def login():
+    """ login """
+    if is_mobie():
+        return render_template("hello/mobile/index.html")
+    else:
+        return render_template("hello/desktop/index.html")
+
+
+@hello.route('/register/', methods=['GET', 'POST'])
+def register():
+    """ register """
+    if is_mobie():
+        return render_template("hello/mobile/index.html")
+    else:
+        return render_template("hello/desktop/index.html")
+
